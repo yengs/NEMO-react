12dcb869e74c4ee8fe2e2a1b4f78d3c067e08cf7
K-sun-H <sunhe.dev@gmail.com>
2022-10-26 18:49:11 +0900

    item start

--- nemo-project/src/App.js
+++ nemo-project/src/App.js
@@ -1,10 +1,13 @@
 import './common.css';
 import Header from './Header';
+import ItemList from './pages/ItemList';
+import ItemUpload from './pages/ItemUpload';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ItemUpload/>
     </div>
   );
 }
