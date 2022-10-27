eebe7f0437953c18c2a95e3d6f82ec591b9219e5
yewon <uhbill@nate.com>
2022-10-27 10:22:23 +0900

    remove Join from app.js

--- nemo-project/src/App.js
+++ nemo-project/src/App.js
@@ -1,12 +1,10 @@
 import './common.css';
 import Header from './Header';
-import Join from './Join';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Join />
     </div>
   );
 }
