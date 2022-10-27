05b9176eafae160d5c9e7ab7dafc8d6cb7e97932
Hyemie <hyemhamgoa1111@gmail.com>
2022-10-27 20:24:37 +0900

    login page

--- nemo-project/src/App.js
+++ nemo-project/src/App.js
@@ -1,13 +1,16 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './common.css';
 import Header from './Header';
-import Login from './LoginPage/Login';
+import Login from './member/Login';
+import { Route } from 'react-router-dom';
 
 function App() {
+  
   return (
-    <div className="App">
-      <Header />
-      <Login />
-    </div>
+      <>
+        <Login />
+      </>
   );
 }
 
