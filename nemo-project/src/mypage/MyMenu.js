85bf0173f5ec00324c0e9662a77b13a7e2be3df5
yewon <uhbill@nate.com>
2022-11-01 19:43:52 +0900

    mystore

--- nemo-project/src/mypage/MyMenu.js
+++ nemo-project/src/mypage/MyMenu.js
@@ -8,8 +8,8 @@ function MyMenu() {
         <div className="myMenuWrap">
             <div className="memberImg"></div>
             <div className="cleanG">
-                클린지수<span>65</span>%
-                <div style={{"width":"100%", "height":"15px", "backgroundColor":"rgb(88, 145, 112)", "borderRadius":"20px"}}></div>
+                클린지수 <span>65</span>%
+                <div style={{"width":"100%", "height":"13px", "backgroundColor":"rgb(53, 77, 119)", "borderRadius":"20px"}}></div>
             </div>
             <div className="menu">
                 <ul>
