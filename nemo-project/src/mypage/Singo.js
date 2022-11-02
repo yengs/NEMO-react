3ca8c2cd029494459a17f07416757abb5c17a161
K-sun-H <sunhe.dev@gmail.com>
2022-11-02 17:49:02 +0900

    신고하기3

--- nemo-project/src/mypage/Singo.js
+++ nemo-project/src/mypage/Singo.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Shirt from '../img/shirt.jpg';
+import ItemSlider from "./ItemSlider.js";
 
 import GoogleLogin from "../img/btn_google_signin_dark_normal_web@2x.png";
 
@@ -12,12 +14,23 @@ function Singo() {
             <div className="regiUserItemList">
                 <h3 className="pageTitle">신고하기</h3>
 
-                <div className="joinWrap memberPage container loginForm">
+                <div className="memberPage2 container loginForm2">
             
             <form>
                 <div className="inputTable">
                     <table>
                         <tbody>
+                        <tr>
+                                <td>사진첨부</td>
+                                
+                            </tr>
+                            <tr>
+                             <td>
+                            
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlTPJg6Tgw-P2brcvdMAxNcnUqT9udTDuSUw&usqp=CAU"/>
+                           
+                             </td>
+                            </tr>
                             <tr>
                                 <td>신고이유</td>
                             </tr>
@@ -42,8 +55,8 @@ function Singo() {
                             <tr>
                             <td>
                                     <div className="btnWrap">
-                                        <input type="submit" className="greenBtn btn" value="신고하기" />
-                                        <input type="submit" className="greenBtn btn" value="신고하기" />
+                                        <input type="submit" className="redBtn2 btn" value="신고하기" />
+                                        <input type="submit" className="grayBtn2 btn" value="취소" />
                                     </div>
                                 </td>
                             </tr>
