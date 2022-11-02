965c82dd7b46c0705a2c1f84ee1555ca2f266be2
yewon <uhbill@nate.com>
2022-11-02 15:14:03 +0900

    타 유저가 보는 내 마이페이지

---
+++ nemo-project/src/mypage/MyMenuForOthers.js
@@ -0,0 +1,20 @@
+import { Link, Route } from 'react-router-dom';
+
+function MyMenu() {
+
+    return (
+        <div className="myMenuWrap">
+            <div className="memberImg"></div>
+            <div className="cleanG">
+                클린지수 <span>65</span>%
+                <div style={{"width":"100%", "height":"13px", "backgroundColor":"rgb(53, 77, 119)", "borderRadius":"20px"}}></div>
+            </div>
+            <div className="menu">
+                <div></div>
+                <button className="warnBtn">신고하기</button>
+            </div>
+        </div>
+    );
+}
+
+export default MyMenu;
