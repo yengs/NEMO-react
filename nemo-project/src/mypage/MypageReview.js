813eb816ce1d9da89efa68819ba0da2650e6dc0a
DuMingK <fragrance_b@naver.com>
2022-11-02 17:47:42 +0900

    dev +

--- nemo-project/src/mypage/MypageReview.js
+++ nemo-project/src/mypage/MypageReview.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import jeans from '../img/jeans.jpg';
-import { Link } from 'react-router-dom';
 
 function MypageReview() {
 
