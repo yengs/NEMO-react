ec19d3f7b2dbdeca9c683b88f7c66169b9d742ef
DuMingK <fragrance_b@naver.com>
2022-11-02 17:44:46 +0900

    중간저장

--- nemo-project/src/mypage/MyPage.js
+++ nemo-project/src/mypage/MyPage.js
@@ -1,14 +1,18 @@
 import MyMenu from "./MyMenu";
 import MyUserStore from "./MyUserStore";
 
-import './mypage.css';
+// import './mypage.css';
+import mypageReview from "./mypageReview.css";
 import { Route } from "react-router-dom";
+import './mypageReview.css';
+import MypageReview from "./MypageReview";
 
 function MyPage() {
     return(
         <div className="myPageWrap">
             <MyMenu />
-            <Route path="/mypage/userstore" component={MyUserStore} exact={true} />
+            {/* <Route path="/mypage/userstore" component={MyUserStore} exact={true} /> */}
+            <MypageReview/>
         </div>
     );
 }
--- nemo-project/src/mypage/MypageReview.js
+++ nemo-project/src/mypage/MypageReview.js
@@ -1,52 +1,106 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from "react-router-dom";
-import Shirt from '../img/shirt.jpg';
+import jeans from '../img/jeans.jpg';
+import { Link } from 'react-router-dom';
 
 function MypageReview() {
 
-    const [myReviewData, setmyReviewData] = useState(''); // 내가 대여한 옷에 대한 후기
+    const [myReviewData, setmyReviewData] = useState(''); // 내가 작성한 후기
     const [yourReviewData, setYourReviewData] = useState(''); // 내가 등록한 상품에 대한 다른 회원의 후기
-    const [itemList, setItemList] = useState(''); // 상품 리스트
+
 
     // 내가 작성한 후기 데이터
     useEffect(() => {
         axios.get('http://localhost:8080/api/review/myReview')
             .then(response => {
                 console.log(response);
-                myReviewData(response.data)
+                setmyReviewData(response.data)
             })
             .catch(error => console.log(error));
     }, []);
 
-    // 상대방이 작성한 내 상품에 대한 후기 데이터 
+    // 내가 등록한 상품에 대한 다른 회원의 후기 데이터
     useEffect(() => {
         axios.get('http://localhost:8080/api/review/yourReview')
             .then(response => {
                 console.log(response);
-                yourReviewData(response.data)
+                setYourReviewData(response.data)
             })
             .catch(error => console.log(error));
     }, []);
 
+
     return (
         <div className="mypageInnerPage">
-            <div className="userReviewListAboutStore">
-                <h2 className="myStoreReviewTitle">내 상점 후기</h2>
-                <hr className='lineB' />
+            <div className="myStoreReview">
+                <div className='reviewTitle'>
+                    <h2 className="reviewTitle">내 상점 후기</h2>
+                    <span><a href="" className='moreReviewDetailPage'>더보기 > </a></span>
+                </div>
+            </div>
+            <hr className='lineH' />
+            <div>
                 <table className="myReviewListAboutStore">
                     <thead>
-                        <tr>
-                            <th colSpan={2}>상품 정보</th>
-                            <th>작성자</th>
-                            <th colSpan={2}>내용</th>
-                        </tr>
+                        <th colSpan={2}>상품 정보</th>
+                        <th>작성자</th>
+                        <th colSpan={2}>내용</th>
                     </thead>
                     <tbody>
                         <tr>
-                            <td rowSpan={3} className="myReviewList">
-                            <div style={{ "backgroundImage": `url(${Shirt})` }}></div>
+                            <td rowSpan={3} className="ReviewItemImageOrigin">
+                                <div style={{ "backgroundImage": `url(${jeans})` }}></div>
                             </td>
+                            <td className='ReviewItemNameOrigin' rowSpan={3} >메종키츠네 셔츠</td>
+                            <td className='ReviewWriter' rowSpan={3}>선희곤듀</td>
+                            <td className='ReviewItemImg'>
+                                <div style={{ "backgroundImage": `url(${jeans})` }}></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className='ReviewContent'>친절하시구 옷 상태도 너무 좋았어요!<br />다음에도 또 거래하고 싶어요</td>
+                        </tr>
+                        <tr>
+                            <td className='satisfing'>
+                                <div>
+                                    만족도 <span>65</span>%
+                                    <div style={{ "width": "100%", "height": "13px", "backgroundColor": "rgb(150,150,150)", "borderRadius": "20px" }}></div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            {/* ------------------------------------------------------------------------------------ */}
+
+            <div className="myStoreReview">
+                <div className='reviewTitle'>
+                    <h2 className="reviewTitle">내 상점 후기</h2>
+                    <span><a href="" className='moreReviewDetailPage'>더보기 > </a></span>
+                </div>
+            </div>
+            <hr className='lineH' />
+            <div>
+                <table className="myReviewListAboutStore">
+                    <thead>
+                        <th colSpan={2}>상품 정보</th>
+                        <th>작성자</th>
+                        <th colSpan={2}>내용</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3} className="ReviewItemImageOrigin">
+                                <div style={{ "backgroundImage": `url(${jeans})` }}></div>
+                            </td>
+                            <td className='ReviewItemNameOrigin' rowSpan={3} >메종키츠네 셔츠</td>
+                            <td className='ReviewWriter' rowSpan={3}>선희곤듀</td>
+                            <td className='ReviewItemImg'>
+                                <div style={{ "backgroundImage": `url(${jeans})` }}></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className='ReviewContent'>친절하시구 옷 상태도 너무 좋았어요!<br />다음에도 또 거래하고 싶어요</td>
                         </tr>
                         <tr>
                             <td className='satisfing'>
