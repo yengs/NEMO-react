bf515f39f4fa797981fbcd46488652289aa7ce13
DuMingK <fragrance_b@naver.com>
2022-11-02 20:24:19 +0900

    img 위치 11/3 변경

--- nemo-project/src/mypage/MypageReview.js
+++ nemo-project/src/mypage/MypageReview.js
@@ -1,45 +1,45 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
 import jeans from '../img/jeans.jpg';
 
 function MypageReview() {
 
-    const [myReviewData, setmyReviewData] = useState(''); // 내가 작성한 후기
-    const [yourReviewData, setYourReviewData] = useState(''); // 내가 등록한 상품에 대한 다른 회원의 후기
+    // const [myReviewData, setmyReviewData] = useState(''); // 내가 작성한 후기
+    // const [yourReviewData, setYourReviewData] = useState(''); // 내가 등록한 상품에 대한 다른 회원의 후기
 
 
-    // 내가 작성한 후기 데이터
-    useEffect(() => {
-        axios.get('http://localhost:8080/api/review/myReview')
-            .then(response => {
-                console.log(response);
-                setmyReviewData(response.data)
-            })
-            .catch(error => console.log(error));
-    }, []);
+    // // 내가 작성한 후기 데이터
+    // useEffect(() => {
+    //     axios.get('http://localhost:8080/api/review/myReview')
+    //         .then(response => {
+    //             console.log(response);
+    //             setmyReviewData(response.data)
+    //         })
+    //         .catch(error => console.log(error));
+    // }, []);
 
-    // 내가 등록한 상품에 대한 다른 회원의 후기 데이터
-    useEffect(() => {
-        axios.get('http://localhost:8080/api/review/yourReview')
-            .then(response => {
-                console.log(response);
-                setYourReviewData(response.data)
-            })
-            .catch(error => console.log(error));
-    }, []);
+    // // 내가 등록한 상품에 대한 다른 회원의 후기 데이터
+    // useEffect(() => {
+    //     axios.get('http://localhost:8080/api/review/yourReview')
+    //         .then(response => {
+    //             console.log(response);
+    //             setYourReviewData(response.data)
+    //         })
+    //         .catch(error => console.log(error));
+    // }, []);
 
 
     return (
         <div className="mypageInnerPage">
             <div className="myStoreReview">
                 <div className='reviewTitle'>
-                    <h2 className="reviewTitle">내 상점 후기</h2>
-                    <span><a href="" className='moreReviewDetailPage'>더보기  </a></span>
+                    <h3 className="reviewTitle">내 상점 후기</h3>
+                    <span><a href={`/review/yourReview`} className='moreReviewDetailPage'>더보기 </a></span>
                 </div>
             </div>
             <hr className='lineH' />
-            <div>
-                <table className="myReviewListAboutStore">
+            <div className='tableWrap'>
+                <table className="yourReviewListAboutStore">
                     <thead>
                         <th colSpan={2}>상품 정보</th>
                         <th>작성자</th>
@@ -57,7 +57,7 @@ function MypageReview() {
                             </td>
                         </tr>
                         <tr>
-                            <td className='ReviewContent'>친절하시구 옷 상태도 너무 좋았어요!<br />다음에도 또 거래하고 싶어요</td>
+                            <td className='ReviewContent' style={{ "padding-top": "0px" }}>친절하시구 옷 상태도 너무 좋았어요!<br />다음에도 또 거래하고 싶어요</td>
                         </tr>
                         <tr>
                             <td className='satisfing'>
@@ -71,20 +71,20 @@ function MypageReview() {
                 </table>
             </div>
 
-            {/* ------------------------------------------------------------------------------------ */}
-        
+            <div className='marging'></div>
+
             <div className="myStoreReview">
                 <div className='reviewTitle'>
-                    <h2 className="reviewTitle">내 상점 후기</h2>
-                    <span><a href="" className='moreReviewDetailPage'>더보기 > </a></span>
+                    <h3 className="reviewTitle">내 작성 후기</h3>
+                    <span><a href={`/review/myReview`} className='moreReviewDetailPage'>더보기 </a></span>
                 </div>
             </div>
             <hr className='lineH' />
-            <div>
-                <table className="myReviewListAboutStore">
+            <div className='tableWrap'>
+                <table className="yourReviewListAboutStore">
                     <thead>
                         <th colSpan={2}>상품 정보</th>
-                        <th>작성자</th>
+                        <th>대여료</th>
                         <th colSpan={2}>내용</th>
                     </thead>
                     <tbody>
@@ -93,13 +93,13 @@ function MypageReview() {
                                 <div style={{ "backgroundImage": `url(${jeans})` }}></div>
                             </td>
                             <td className='ReviewItemNameOrigin' rowSpan={3} >메종키츠네 셔츠</td>
-                            <td className='ReviewWriter' rowSpan={3}>선희곤듀</td>
+                            <td className='ReviewWriter' rowSpan={3}>70,000</td>
                             <td className='ReviewItemImg'>
                                 <div style={{ "backgroundImage": `url(${jeans})` }}></div>
                             </td>
                         </tr>
                         <tr>
-                            <td className='ReviewContent'>친절하시구 옷 상태도 너무 좋았어요!<br />다음에도 또 거래하고 싶어요</td>
+                            <td className='ReviewContent' style={{ "padding-top": "0px" }}>친절하시구 옷 상태도 너무 좋았어요!<br />다음에도 또 거래하고 싶어요</td>
                         </tr>
                         <tr>
                             <td className='satisfing'>
@@ -112,7 +112,7 @@ function MypageReview() {
                     </tbody>
                 </table>
             </div>
-        </div>
+            </div>
     );
 }
 
