aa45d05e990b5a0c70ae70ab134a701ad588fc69
DuMingK <fragrance_b@naver.com>
2022-11-03 12:33:50 +0900

    cssNameFullchangeSave.ver

--- nemo-project/src/mypage/MyPage.js
+++ nemo-project/src/mypage/MyPage.js
@@ -1,18 +1,17 @@
 import MyMenu from "./MyMenu";
 import MyUserStore from "./MyUserStore";
+import { Route } from "react-router-dom";
+import MypageReview from "./MypageReview";
 
 import './mypage.css';
-import mypageReview from "./mypageReview.css";
-import { Route } from "react-router-dom";
 import './mypageReview.css';
-import MypageReview from "./MypageReview";
 
 function MyPage() {
     return(
         <div className="myPageWrap">
             <MyMenu />
             <Route path="/mypage/userstore" component={MyUserStore} exact={true} />
-            <MypageReview/>
+            <Route path="/mypage/review" component={MypageReview} exact={true} />
         </div>
     );
 }
