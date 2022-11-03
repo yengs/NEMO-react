cf16e6da6d449b7cb46b9edea6b907849a0978dc
yewon <uhbill@nate.com>
2022-11-03 21:43:42 +0900

    route + link + css update

--- nemo-project/src/mypage/MyMenu.js
+++ nemo-project/src/mypage/MyMenu.js
@@ -12,10 +12,10 @@ function MyMenu() {
             <div className="menu">
                 <ul>
                     {/* <li>나의 계정 설정</li> */}
-                    <li><Link to="/mypage/mypageitem">내 스토어</Link></li>
-                    <li>등록상품 조회</li>
+                    <li><Link to="/mypage/mybooking">내 대여이력</Link></li>
+                    <li><Link to="/mypage/mypageitem">등록상품 조회</Link></li>
                     <li><Link to="/mypage/review">후기 조회</Link></li>
-                    <li>회원정보 수정</li>
+                    <li><Link to="/mypage/userupdate">회원정보 수정</Link></li>
                 </ul>
             </div>
         </div>
