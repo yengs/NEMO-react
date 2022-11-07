52fedaf99bd715514dbf9971c4a295bab37acad0
yewon <uhbill@nate.com>
2022-11-07 11:13:55 +0900

    마이페이지 메뉴 너비 수정

--- nemo-project/src/mypage/Singo.js
+++ nemo-project/src/mypage/Singo.js
@@ -17,7 +17,7 @@ function Singo({ history }) {
 
 
     return (
-        <SingoContainer>
+        <SingoContainer style={{width:'100%', height:'100%'}}>
             <div className="mypageInnerPage">
                 <div className="regiUserItemList">
                     <h3 className="pageTitle">신고하기</h3>
