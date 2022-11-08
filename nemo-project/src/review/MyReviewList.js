f28702dffba53e9bb0db896b2ab6ce8e0a556432
leessg <leesg812@gmail.com>
2022-11-08 11:22:08 +0900

    페이징

--- nemo-project/src/review/MyReviewList.js
+++ nemo-project/src/review/MyReviewList.js
@@ -1,31 +1,36 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState   } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import "./reviewDetail.css";
 import Shirt from '../img/shirt.jpg';
+import Paging from "../pagination/Paging";
+
 
 function MyReviewList() {
 
-    const [datas, setDatas] = useState([]);
+    const ITEM_COUNT_PER_PAGE = 10; 
+
+    const [ datas, setDatas ] = useState([]);         // 리뷰 전체 데이터
+    const [ count, setCount ] = useState(0);        // 전체 개수
+    const [ page, setPage ] = useState(1);          // 보여지는 페이지
+    const [ items, setItems ] = useState([]);       // 페이징을 통해서 보여줄 데이터
 
     useEffect(() => {
         axios.get('http://localhost:8080/api/review/myReview')
             .then(response => {
                 console.log(response);
-                setDatas(response.data)
+                setDatas(response.data);    // 리뷰 전체 데이터 설정
+                setCount(response.data.length);
+                setItems(response.data.slice((page - 1) * ITEM_COUNT_PER_PAGE, page * ITEM_COUNT_PER_PAGE));
+                
             })
             .catch(error => console.log(error));
     }, []);
 
-    useEffect(() => {
-        axios.get('http://localhost:8080/api/review/myReview')
-            .then(response => {
-                console.log(response);
-                setDatas(response.data)
-            })
-            .catch(error => console.log(error));
-    }, []);
-
+    const changePage = page => {
+        setPage(page);
+        setItems(datas.slice((page - 1) * ITEM_COUNT_PER_PAGE, page * ITEM_COUNT_PER_PAGE));
+    };
 
     return (
         <>
@@ -49,7 +54,7 @@ function MyReviewList() {
                     </thead>
                     <tbody>
                         {
-                            datas && datas.map(review => (
+                            items && items.map(review => (
                                 <tr key={review.reviewNum}>
                                     <td>{review.reviewNum}</td>
                                     {/* <td>{review.reviewImage}</td> */}
@@ -74,6 +79,9 @@ function MyReviewList() {
                         }
                     </tbody>
                 </table>
+                <div>
+                    <Paging page={page} count={count} setPage={changePage} />
+                </div>
             </div>
         </>
     );
